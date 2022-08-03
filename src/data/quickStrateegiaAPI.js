ce801702b1c5dc039ee5ca867cb47f94f779f46f
filipecalegario <fcac@cin.ufpe.br>
2022-08-02 22:45:20 -0300

    initial commit

---
+++ src/data/quickStrateegiaAPI.js
@@ -0,0 +1,16 @@
+const API_URL_NOTIFICATION = "https://api.strateegia.digital/notifications/v1/"
+
+export async function getFlowNotifications(token) {
+
+  const response = await fetch(`${API_URL_NOTIFICATION}flow?size=10&sort=true&page=0`, {
+    method: 'get',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    }
+  });
+
+  const data = await response.json();
+
+  return data;
+}
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,83 +1,75 @@
 import {
   Box,
   Heading,
-  Link,
-  ListItem,
-  Text,
+  Select,
   UnorderedList,
+  ListItem,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
-import * as api from "strateegia-api";
 import Loading from "../components/Loading";
-import MapList from "../components/MapList";
-import ProjectList from "../components/ProjectList";
-import DivPointList from "../components/DivPointList";
+import { getFlowNotifications } from "../data/quickStrateegiaAPI";
 
 export default function Main() {
   const [selectedProject, setSelectedProject] = useState("");
-  const [selectedMap, setSelectedMap] = useState("");
-  const [selectedDivPoint, setSelectedDivPoint] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [accessToken, setAccessToken] = useState("");
-  const [mapDetails, setMapDetails] = useState(null);
-  const [firstMap, setFirstMap] = useState(null);
+  const [projectList, setProjectList] = useState(null);
+  const [flowData, setFlowData] = useState(null);
+  const [selectedFlow, setSelectedFlow] = useState(null);
 
   const handleSelectChange = (e) => {
     setSelectedProject(e.target.value);
-    async function fetchMapList() {
-      try {
-        const accessToken = localStorage.getItem("accessToken");
-        const project = await api.getProjectById(accessToken, e.target.value);
-        setFirstMap(project.maps[0].id);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    fetchMapList();
-  };
-
-  const handleMapSelectChange = (e) => {
-    setSelectedMap(e.target.value);
-  };
-
-  const handleDivPointSelectChange = (e) => {
-    setSelectedDivPoint(e.target.value);
   };
 
   useEffect(() => {
-    setMapDetails(null);
-    setSelectedMap("");
-    setSelectedDivPoint("");
+    const selectedContentByProject = flowData?.content.filter(
+      (item) => item.context.project.id === selectedProject
+    );
+    setSelectedFlow(selectedContentByProject);
   }, [selectedProject]);
 
   useEffect(() => {
-    setSelectedDivPoint("");
-    async function fetchData() {
+    async function fetchFlow() {
       setIsLoading(true);
       try {
-        const response = await api.getMapById(accessToken, selectedMap);
-        setMapDetails({ ...response });
-        // [TODO] - use the access token to fetch the data
-        // [TODO] - add the fetch data function here
+        const flowData_ = await getFlowNotifications(accessToken_);
+        console.log("flow %o", flowData_);
+        setFlowData(flowData_);
+        const projects = flowData_.content.map((item) => {
+          return {
+            id: item.context.project.id,
+            title: item.context.project.title,
+          };
+        });
+        console.log("projects %o", projects);
+        const distinctProjects = [
+          ...new Set(projects.map((obj) => obj.id)),
+        ].map((id) => {
+          return projects.find((obj) => obj.id === id);
+        });
+        setProjectList(distinctProjects);
+        console.log("distinct projects %o", distinctProjects);
+        const idFromSelection = "601a83d4cf364315a4cb9814";
+        const idFromSelection2 = "6115ca0b95aeec1babf1c589";
+        const selectedContentByProject = flowData_.content.filter(
+          (item) => item.context.project.id === idFromSelection
+        );
+        console.log("selected projects %o", selectedContentByProject);
+        setIsLoading(false);
       } catch (error) {
         console.log(error);
       }
-      setIsLoading(false);
     }
-    fetchData();
-  }, [selectedMap]);
-
-  useEffect(() => {
-    setAccessToken(localStorage.getItem("accessToken"));
+    const accessToken_ = localStorage.getItem("accessToken");
+    fetchFlow();
+    setAccessToken(accessToken_);
   }, []);
-
-  console.log(selectedProject + " , " + firstMap);
 
   return (
     <Box padding={10}>
       <Box display="flex">
-        <ProjectList handleSelectChange={handleSelectChange} />
-        <Link
+        {/* <ProjectList handleSelectChange={handleSelectChange} /> */}
+        {/* <Link
           href={`https://app.strateegia.digital/journey/${selectedProject}/map/${firstMap}`}
           target="_blank"
           bg="#E9ECEF"
@@ -90,45 +82,40 @@ export default function Main() {
           justifyContent="center"
         >
           link para a jornada
-        </Link>
+        </Link> */}
+        <Select
+          placeholder="escolha o projeto"
+          onChange={handleSelectChange}
+          borderRadius={"6px 0 0 6px"}
+        >
+          {projectList &&
+            projectList.map((item) => {
+              return (
+                <option key={item.id} value={item.id}>
+                  {item.title}
+                </option>
+              );
+            })}
+        </Select>
       </Box>
-      <MapList
-        projectId={selectedProject}
-        handleSelectChange={handleMapSelectChange}
-      />
-      <DivPointList
-        mapId={selectedMap}
-        handleSelectChange={handleDivPointSelectChange}
-      />
       <Loading active={isLoading} />
-      <Heading as="h3" size="md" mb={3} mt={3}>
-        [applet title here]
-      </Heading>
-      {/* [TODO] Add you component here */}
-      {mapDetails?.points ? (
-        <Box mt={3}>
-          <Heading as="h3" size="md" mb={3}>
-            Map Details
-          </Heading>
-          <Text>Title: {mapDetails?.title}</Text>
-          <Text>ID: {mapDetails?.id}</Text>
-          <Text>Created at: {mapDetails?.created_at}</Text>
-          <Text>Points: {mapDetails?.points?.length}</Text>
-          <UnorderedList>
-            {mapDetails?.points?.map((point) => (
-              <ListItem>
-                {point.point_type}:{" "}
-                {point.point_type === "CONVERSATION"
-                  ? point.description
-                  : point.title}
-              </ListItem>
-            ))}
-          </UnorderedList>
-        </Box>
-      ) : null}
-      {selectedDivPoint !== "" ? (
-        <Text> Selected Divergence Point: {selectedDivPoint}</Text>
-      ) : null}
+      <Box margin={10}>
+        <UnorderedList margin={5}>
+          {selectedFlow?.map((item) => (
+            <ListItem key={item.id}>
+              <strong>event type:</strong> {item.event_type}
+              <br></br>
+              <strong>author:</strong> {item.payload.author.name}
+              <br></br>
+              <strong>updated at:</strong> {item.payload.updated_at}
+              <br></br>
+              <strong>parent:</strong> {item.payload.parent?.text}
+              <br></br>
+              <strong>child:</strong> {item.payload.text}
+            </ListItem>
+          ))}
+        </UnorderedList>
+      </Box>
     </Box>
   );
 }
